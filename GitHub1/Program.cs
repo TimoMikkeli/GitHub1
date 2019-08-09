82794157e22437e1b97a2bfc368b98ee83b96d3c
htimy01 <htimy01@MLHTIMY01>
2019-08-09 14:37:21 +0300

    First modification

--- GitHub1/Program.cs
+++ GitHub1/Program.cs
@@ -6,7 +6,7 @@ namespace GitHub1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Timpe!");
         }
     }
 }
